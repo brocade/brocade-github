cf4659b096494bb0994d17cf69fe11986ba7de13
Gary Berger <gberger@brocade.com>
2016-02-17 12:09:22 -0500

    Make Lein version 2.5.1 to accompdate TravisCI
    Signed-off-by: Gary Berger <gberger@brocade.com>

--- brocade/project.clj
+++ brocade/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :min-lein-version "2.5.3"
+  :min-lein-version "2.5.1"
 
 
   :dependencies [[org.clojure/clojure "1.7.0"]
